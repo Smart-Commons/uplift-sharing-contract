73f11c7deb665d34a672f7a64c34dce900a09b35
Oguzhan Yayla <oguzhan@oyayla.com>
2019-12-17 16:19:17 +0100

    Basic contract and its tests

--- src/contracts/SmartCommons.sol
+++ src/contracts/SmartCommons.sol
@@ -2,6 +2,43 @@ pragma solidity ^0.5.0;
 
 contract SmartCommons {
     string public name;
+    uint public propertySaleCount = 0;
+    mapping(uint => PropertySale) public property_sales;
+
+
+    struct PropertySale {
+        uint id;
+        string name;
+        uint price;
+        uint uplift_value;
+        uint uplift_cont_rate;
+        address owner;
+        bool purchased;
+        string location;
+    }
+
+    event PropertySaleCreated(
+        uint id,
+        string name,
+        uint price,
+        uint uplift_value,
+        uint uplift_cont_rate,
+        address owner,
+        bool purchased
+    );
+
+    function createPropertySale(string memory _name, uint _price, uint _uplift_value, uint _uplift_cont_rate, string memory _location) public {
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+        require(_uplift_value > 0);
+        require(_uplift_cont_rate > 0);
+
+        propertySaleCount ++;
+        property_sales[propertySaleCount] = PropertySale(propertySaleCount, _name, _price, _uplift_value, _uplift_cont_rate, msg.sender, false, _location);
+        emit PropertySaleCreated(propertySaleCount, _name, _price, _uplift_value, _uplift_cont_rate, msg.sender, false);
+    }
+
+
 
     constructor() public {
         name = "Smart Commons";
--- test/SmartCommons.test.js
+++ test/SmartCommons.test.js
@@ -1,6 +1,9 @@
 const Smartcommons = artifacts.require('./SmartCommons.sol')
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
 
-contract('SmartCommons', (accounts) => {
+contract('SmartCommons', ([deployer, seller, buyer]) => {
   let smartcommons
 
   before(async () => {
@@ -20,6 +23,26 @@ contract('SmartCommons', (accounts) => {
       const name = await smartcommons.name()
       assert.equal(name, 'Smart Commons')
     })
+  })
 
+  describe('propertySales', async () => {
+      let result, propertySaleCount
+
+      before(async () => {
+          result = await smartcommons.createPropertySale('Block 8th Apt.', 300000, 200000, 10, 'Berlin', { from: seller })
+          propertySaleCount = await smartcommons.propertySaleCount()
+      })
+
+      it('creates propertySale', async () => {
+            assert.equal(propertySaleCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), propertySaleCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'Block 8th Apt.', 'name is correct')
+            assert.equal(event.price, '300000', 'price is correct')
+            assert.equal(event.uplift_value, '200000', 'uplift value is correct')
+            assert.equal(event.uplift_cont_rate, '10', 'uplift cont rate is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, 'purchased is correct')
+      })
   })
 })
