e0f9b7d3fc13eb7ed7651ad390a4a230cee12c90
Oguzhan Yayla <oguzhan@oyayla.com>
2019-12-19 12:35:26 +0100

    Nested structs for relationships.

--- src/contracts/SmartCommons.sol
+++ src/contracts/SmartCommons.sol
@@ -2,8 +2,55 @@ pragma solidity ^0.5.0;
 
 contract SmartCommons {
     string public name;
+ 
     uint public propertySaleCount = 0;
     mapping(uint => PropertySale) public property_sales;
+
+    struct Member {
+        uint id;
+        string name;
+        mapping(uint => Property) properties;
+    }
+    uint public memberCount = 0;
+    mapping(uint => Member) public members;
+
+    function createMember(string memory _name) public {
+        memberCount++;
+        members[memberCount++] = Member(memberCount, _name);
+    }
+
+    struct Property {
+        uint id;
+        string name;
+        string location;
+        uint price;
+    }
+    uint public propertyCount = 0;
+    mapping(uint => Property) public properties;
+    
+    event PropertyCreated (
+        uint id,
+        string name,
+        string location,
+        uint price
+    );
+
+    function newProperty(string memory _name, string memory _location, uint _price) public {
+        require(bytes(_name).length > 0);
+        require(bytes(_location).length > 0);
+        require(_price > 0);
+        
+        propertyCount++;
+        properties[propertyCount] = Property(propertyCount, _name, _location, _price);
+        emit PropertyCreated(propertyCount, _name, _location, _price);
+    }
+
+    struct InvestmentFund {
+        address fund;
+        uint total_amount;
+        mapping (uint => Member) members;
+    }
+    uint public totalInvestmentFund = 0;
 
 
     struct PropertySale {
